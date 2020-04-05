7ecd628f143f3d1a7b6ded3c0e2b6a0abe5cf7f4
a188037445 <188037445@qq.com>
2020-04-05 09:18:31 +0800

    点文件优先展示

--- bin/src/util.dart
+++ bin/src/util.dart
@@ -10,7 +10,23 @@ bool isFileHidden(String path) {
 
 int comparePath(String a, String b) {
   final minLen = a.length < b.length ? a.length : b.length;
-  for (var i = 0; i < minLen; i++) {
+  if (minLen == 0) {
+    return a.length - b.length;
+  }
+  final aCode = a.codeUnitAt(0);
+  final bCode = b.codeUnitAt(0);
+  var start = 0;
+  if (aCode == bCode) {
+    start = 1;
+  } else {
+    if (isDot(aCode)) {
+      return -1;
+    }
+    if (isDot(bCode)) {
+      return 1;
+    }
+  }
+  for (var i = start; i < minLen; i++) {
     var aCode = a.codeUnitAt(i);
     var bCode = b.codeUnitAt(i);
     if (aCode == bCode) {
@@ -59,6 +75,8 @@ int parseInt(String s, int startIndex, int endIndex) {
 bool isLetter(int ascii) => ascii >= 65 && ascii <= 90 || ascii >= 97 && ascii <= 122;
 
 bool isNumber(int ascii) => ascii >= 48 && ascii <= 57;
+
+bool isDot(int ascii) => ascii == 46;
 
 // 返回第一个不是数字的索引
 int checkLastNumber(String str, int index) {
